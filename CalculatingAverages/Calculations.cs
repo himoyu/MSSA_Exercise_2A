2014f6de04fb1493fc1fef53b9495d45af02b36c
Jeff Adams <atomicxistence@gmail.com>
2019-02-22 22:51:29 -0500

    add calculations functionality

---
+++ CalculatingAverages/Calculations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculatingAverages
+{
+    class Calculations
+    {
+        public int Sum(List<int> numbers)
+        {
+            int total = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                total += numbers[i];
+            }
+
+            return total;
+        }
+
+        public int Average(int sum, int amount)
+        {
+            return sum / amount;
+        }
+
+        public string Grade(int gradeValue)
+        {
+            switch(gradeValue)
+            {
+                case int value when value > 89:
+                return "A";
+                case int value when value < 90 && value > 79:
+                return "B";
+                case int value when value < 80 && value > 69:
+                return "C";
+                case int value when value < 70 && value > 59:
+                return "D";
+                case int value when value < 60:
+                return "F";
+                default:
+                return null;
+            }
+        }
+    }
+}
