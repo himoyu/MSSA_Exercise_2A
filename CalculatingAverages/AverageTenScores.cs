42bd3049a430349d18da6349f494ca6800bb9c06
Jeff Adams <atomicxistence@gmail.com>
2019-02-24 13:27:56 -0500

    Added functionality to AverageTenScores applet

--- CalculatingAverages/AverageTenScores.cs
+++ CalculatingAverages/AverageTenScores.cs
@@ -15,7 +15,19 @@ namespace CalculatingAverages
 
         public override void Run()
         {
-            
+            display = new Display(title, totalRows, Program.colorPreset);
+            var calculations = new Calculations();
+
+             do
+            {
+                var numbers = AskForInput(10);
+                var sum = calculations.Sum(numbers);
+                var answer = calculations.Average(sum, numbers.Count);
+                var grade = calculations.Grade(answer);
+
+                display.SingleLine($"The average grade of your scores is a(n) {grade}, ({answer}%).","Press ENTER to continue");
+
+            } while (RunAgain());
         }
     }
 }
